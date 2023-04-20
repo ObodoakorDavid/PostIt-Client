b3d9da1d6ac07086c0db434d606d81709e96c409
ObodoakorDavid <obodoakordavid@outlook.com>
2023-04-20 14:07:35 +0100

    dashboard now working

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const baseURL = "http://127.0.0.1:8000/api/v1";
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   let login = async (body) => {
     let response = await fetch(`${baseURL}/auth/token/login/`, {
@@ -30,15 +30,20 @@ export const AuthProvider = ({ children }) => {
     });
     let data = await response.json();
     console.log(data);
+
     setToken(data.auth_token);
+
     localStorage.setItem("token", JSON.stringify(data.auth_token));
+    if (data.auth_token) {
+      navigate("/dashboard");
+    }
   };
 
   let getUser = async () => {
     let response = await fetch(`${baseURL}/auth/users/me`, {
       method: "GET",
       headers: {
-        Authorization: `Token ${token}`,
+        "Authorization": `Token ${token}`,
         "Content-Type": "application/json",
       },
     });
@@ -46,8 +51,6 @@ export const AuthProvider = ({ children }) => {
     console.log(data);
     setUser(data);
   };
-
-
 
   useEffect(() => {
     console.log(token);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,24 @@
 /** @format */
 
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import RootLayout from "../layouts/RootLayout";
 import AuthContext from "../context/AuthContext";
-
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const { getUser, user } = useContext(AuthContext);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getUser();
+    if (!user){
+      navigate('/login')
+    }
+  }, []);
+
   return (
     <RootLayout>
-      <h1>Welcome Maria</h1>
+      <h1>Welcome {user && user.username}</h1>
     </RootLayout>
   );
 };
