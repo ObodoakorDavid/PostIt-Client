03dd580f50deb2f9b5210b18eec934a7cbf144b3
ObodoakorDavid <obodoakordavid@outlook.com>
2023-04-30 08:24:45 +0100

    edit story almost done

--- src/pages/CreateStory.jsx
+++ src/pages/CreateStory.jsx
@@ -1,25 +1,84 @@
 /** @format */
 
-import React from "react";
+import React, { useContext } from "react";
 import RootLayout from "../layouts/RootLayout";
 import editIcon from "../assets/images/edit.png";
+import { useForm } from "react-hook-form";
+import AuthContext from "../context/AuthContext";
 
 const CreateStory = () => {
+  const { token, baseURL } = useContext(AuthContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log("working");
+    console.log(errors);
+    setTimeout(async () => {
+      let response = await fetch(`${baseURL}/stories/create/`, {
+        method: "POST",
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+      }
+      let data1 = await response.json();
+      console.log(data1);
+    }, 1000);
+  };
+
   return (
     <RootLayout>
       <div className="px-4 text-sm-start">
         <h1 className="py-3 fw-bold">Create Story</h1>
-        <form className="d-flex flex-column gap-4 pb-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="d-flex flex-column gap-4 pb-5"
+        >
           <div className=" position-relative">
-            <img className=" position-absolute bg-white p-1" src={editIcon} alt="" />
-            <input className="w-100 px-4 py-2 fw-semibold border rounded-1" type="text" placeholder="Title" />
+            <img
+              className=" position-absolute bg-white p-1"
+              src={editIcon}
+              alt=""
+            />
+            <input
+              className="w-100 px-4 py-2 fw-semibold border rounded-1"
+              type="text"
+              placeholder="Title"
+              {...register("title", {
+                required: true,
+              })}
+            />
           </div>
           <div className=" position-relative">
-            <img className=" position-absolute bg-white p-1" src={editIcon} alt="" />
-            <input className="w-100 px-4 py-2 fw-semibold border rounded-1" type="text" placeholder="Tags" />
+            <img
+              className=" position-absolute bg-white p-1"
+              src={editIcon}
+              alt=""
+            />
+            <input
+              className="w-100 px-4 py-2 fw-semibold border rounded-1"
+              type="text"
+              placeholder="Tags"
+              {...register("tags", {
+                required: true,
+              })}
+            />
           </div>
           <div className=" position-relative">
-            <img className=" position-absolute bg-white p-1" src={editIcon} alt="" />
+            <img
+              className=" position-absolute bg-white p-1"
+              src={editIcon}
+              alt=""
+            />
             <textarea
               className="w-100 px-4 py-2 fw-semibold border rounded-1"
               name=""
@@ -27,9 +86,14 @@ const CreateStory = () => {
               cols="30"
               rows="10"
               placeholder="Write your story......"
+              {...register("story", {
+                required: true,
+              })}
             ></textarea>
           </div>
-          <button className="btn btn-bg-main w-50 mx-auto text-white">Publish Story</button>
+          <button className="btn btn-bg-main w-50 mx-auto text-white">
+            Publish Story
+          </button>
         </form>
       </div>
     </RootLayout>
--- src/pages/EditStory.jsx
+++ src/pages/EditStory.jsx
@@ -1,19 +1,68 @@
 /** @format */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import RootLayout from "../layouts/RootLayout";
 import { useParams } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import { useForm } from "react-hook-form";
+import { useFetch } from "../hooks/useFetch";
+import toast from "react-hot-toast";
 
 const EditStory = () => {
+  const { login, authenticating, token, baseURL } = useContext(AuthContext);
   const { id } = useParams();
+
+  const { data, loading, error } = useFetch(`${baseURL}/stories/${id}/`, token);
+
+  const [title, setTitle] = useState(!loading ? data.title : "");
+  const [tags, setTags] = useState("");
+  const [story, setStory] = useState("");
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
+
   useEffect(() => {
-    console.log(id);
-  });
+    data && setTitle(data.title);
+    data && setTags(data.tags);
+    data && setStory(data.story);
+  }, [loading]);
+
+  const updateStory = async () => {
+    const body = {
+      title,
+      tags,
+      story,
+    };
+    let response = await fetch(`${baseURL}/stories/${id}/`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json()
+    console.log(data);
+    if (response.ok){
+      toast.success('done')
+
+    }
+
+   
+  };
 
   return (
     <RootLayout>
       <div className="px-4 text-sm-start update-story">
-        <form className="d-flex flex-column gap-4 pb-5">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            updateStory()
+          }}
+          className="d-flex flex-column gap-4 pb-5"
+        >
           <div className="d-flex align-items-center border rounded-1 px-2 py-1 ">
             <label className="fw-bolder text-secondary" htmlFor="title">
               Title:
@@ -22,6 +71,8 @@ const EditStory = () => {
               id="title"
               className="w-100 px-1 py-2 fw-semibold border-0"
               type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div className="d-flex align-items-center border rounded-1 px-2 py-1">
@@ -32,6 +83,8 @@ const EditStory = () => {
               id="tags"
               className="w-100 px-1 py-2 fw-semibold border-0"
               type="text"
+              value={tags}
+              onChange={(e) => setTags(e.target.value)}
             />
           </div>
           <div>
@@ -41,6 +94,8 @@ const EditStory = () => {
               id=""
               cols="30"
               rows="10"
+              value={story}
+              onChange={(e) => setStory(e.target.value)}
             ></textarea>
           </div>
           <button className="btn btn-bg-main w-50 mx-auto text-white">
